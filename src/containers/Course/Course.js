db1d879e509c91e3af227acac4579369b1ad1ab4
Darklink <passakorndark@gmail.com>
2020-05-23 15:55:40 +0700

    Create course routing

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+export class Course extends Component {
+  render() {
+    return (
+      <Container>
+        <Row >
+          <Col>
+            Hello World!!!
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
